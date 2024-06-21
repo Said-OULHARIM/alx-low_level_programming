a9677c1efa7d837c0bf7b0a57b2a2c9573b89fac
Said-Oulharim <soulharim@gmail.com>
2024-06-20 21:06:15 -0700

    bonus part

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point - expecting int return
+ *
+ * Return: We inputted the int 1 to return if code id good
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
