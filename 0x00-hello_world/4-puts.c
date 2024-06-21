915e41b3323d09d5ec5b4ed3b9e87176807bf56a
Said-Oulharim <soulharim@gmail.com>
2024-06-20 21:00:42 -0700

    updated 4 puts betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  * puts:  prints the string inside it
  * Return: int 0 if there are no errors
  */
-int main(){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
