fb0738b18c43fefe96adbb07dab9529437290126
Said-Oulharim <soulharim@gmail.com>
2024-06-21 16:56:47 -0700

    update task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,9 +6,10 @@
 */
 int main(void)
 {
-	int a;
-	for (a = 0 ; a < 10 ; a++)
-		printf("%d", a);
-	putchar('\n');
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+	printf("\n");
 	return (0);
 }
