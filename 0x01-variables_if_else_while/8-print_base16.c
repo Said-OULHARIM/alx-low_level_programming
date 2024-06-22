213ef6a14dcbf2770cdfb8956e453087d8533552
Said-Oulharim <soulharim@gmail.com>
2024-06-21 17:31:11 -0700

    update 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,16 +3,16 @@
 /**
 * main - Print the numbers in hexidecimal
 *
-* Return 0 
+* Return: 0 
 */
 int main(void)
 {
-		int integer;
+	int integer;
 
-		for (integer = '0'; integer <= '9'; integer++)
-		putchar(integer);
-		for (integer = 'a'; integer <= 'f'; integer++)
-		putchar(integer);
-		putchar('\n');
+	for (integer = '0'; integer <= '9'; integer++)
+	putchar(integer);
+	for (integer = 'a'; integer <= 'f'; integer++)
+	putchar(integer);
+	putchar('\n');
 	return (0);
 }
