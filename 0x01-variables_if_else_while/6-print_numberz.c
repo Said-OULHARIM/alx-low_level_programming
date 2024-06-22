75b837f0bc243e8ff597811e5ae837ec3379ccb7
Said-Oulharim <soulharim@gmail.com>
2024-06-21 17:01:03 -0700

    update task -

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,13 @@
 
 /**
 * main - Print numbers from 0 to 9
-* with onlu putchar
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	for (i = 0; i < 10; i++)
 	putchar(i + '0');
-
 	putchar('\n');
-
 	return (0);
 }
