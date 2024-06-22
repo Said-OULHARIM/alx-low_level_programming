a8c466d6d5278d3171da83190b75a7be27bf13fb
Said-Oulharim <soulharim@gmail.com>
2024-06-21 17:36:06 -0700

    update 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@ int main(void)
 	int integer;
 
 	for (integer = '0'; integer <= '9'; integer++)
+	{
 	putchar(integer);
+	}
 	for (integer = 'a'; integer <= 'f'; integer++)
+	{
 	putchar(integer);
+	}
 	putchar('\n');
 	return (0);
 }
