22caaf1d3424dd88afbebb1a3e4fa57c6d135637
Said-Oulharim <soulharim@gmail.com>
2024-06-21 17:10:32 -0700

    update task -

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - Print numbers from 0 to 9
 * Return: Always 0 (Success)
@@ -7,7 +6,6 @@
 int main(void)
 {
 	int i;
-
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
