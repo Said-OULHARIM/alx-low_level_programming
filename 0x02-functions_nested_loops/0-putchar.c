f15c6ee777dcce689b9f754b50b6abd981a15ec3
Said-Oulharim <soulharim@gmail.com>
2024-06-26 03:45:57 -0700

    updated 0-putchar.c for betty linter errors

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,16 @@
 #include <unistd.h>
 
 /*
- *main point of entry
- *using write to print the "_putchar"
- *return 0 on success (always)
+ *Main - main point of entry
+ *Description: using write to print the "_putchar"
+ *Return: 0 on success (always)
  */
-int main (void)
+int main(void)
 {
 	char string[] = "_putchar";
 	int i;
-	for (i=0;i<8;i++)
+
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(string[i]);
 	}
