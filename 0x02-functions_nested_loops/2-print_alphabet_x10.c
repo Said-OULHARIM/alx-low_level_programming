9dcbe143cc317835a042107223193079b1676a4b
Said-Oulharim <soulharim@gmail.com>
2024-06-26 06:52:42 -0700

    updated 2-printx10 - loop typo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- *print_alphabet - main point of entry
+ *print_alphabet_x10 - main point of entry
  *
  *Return: Always 0 (Success)
  */
@@ -11,12 +11,12 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 0; i < 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 97; i < 123; i++)
 		{
 			_putchar(i);
 		}
 	_putchar('\n');
-	}	
+	}
 }
